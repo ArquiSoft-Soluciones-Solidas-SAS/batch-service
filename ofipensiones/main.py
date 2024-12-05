018f1c1b958c187e3155c985fb59cd36ae8ce53b
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-12-05 10:30:20 -0500

    batch

--- ofipensiones/main.py
+++ ofipensiones/main.py
@@ -1,33 +1,21 @@
-import uvicorn
+import datetime
 from fastapi import FastAPI
-from apscheduler.schedulers.background import BackgroundScheduler
-from batch.report_generator import generate_reports
 from contextlib import asynccontextmanager
+from apscheduler.schedulers.background import BackgroundScheduler
 
-# Crear la aplicación FastAPI
-app = FastAPI()
+def printit():
+    print(datetime.datetime.now())
 
-# Configurar APScheduler
-scheduler = BackgroundScheduler()
-scheduler.add_job(generate_reports, "cron", minute="*")
-scheduler.start()
 
-# Usar un manejador de ciclo de vida en lugar de @app.on_event
 @asynccontextmanager
-async def lifespan(app: FastAPI):
-    try:
-        yield
-    finally:
-        scheduler.shutdown(wait=False)
+async def lifespan(app:FastAPI):
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(printit,"interval",minutes = 1)
+    scheduler.start()
+    yield
 
-# Configurar FastAPI con el ciclo de vida
 app = FastAPI(lifespan=lifespan)
 
-# Ruta para comprobar el estado del servicio
 @app.get("/")
-async def read_root():
-    return {"message": "API activa y cronjob configurado."}
-
-# Punto de entrada principal
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+async def test():
+    return "Ok"
